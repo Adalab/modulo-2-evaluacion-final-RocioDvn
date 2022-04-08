244e4d25ab05c0132249b7232280ba10fbd4682a
Rocio Del Villar <rocio.delvillar@gmail.com>
2022-04-08 15:25:32 +0200

    html js start

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const input = document.querySelector('.js-input');
+const btnSearch = document.querySelector('.js-btnSearch');
+const btnReset = document.querySelector('.js-btnReset');
+const favoriteDrinks = document.querySelector('.js-favoriteDrinks');
+const totalDrinks = document.querySelector('.js-totalDrinks');
